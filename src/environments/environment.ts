a7689fcb2ca60f9861b17a6b32c9b724d0a938b1
Anthony Londoño <148740242+4n-ch4n@users.noreply.github.com>
2025-03-21 18:36:46 -0500

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: 'true',
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
-  apiKey: 'ntIUVNBVnaqgCCX26hD95Zq20uJZzzPz',
-  giphyUrl: 'https://api.giphy.com/v1',
-};
